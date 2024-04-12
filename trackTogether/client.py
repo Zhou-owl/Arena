b8678b1e3917c6f66672aacaedc8557af74bfc1c
Yongqi <54852826+Zhou-owl@users.noreply.github.com>
2024-04-12 17:38:47 -0400

    framework

---
+++ trackTogether/client.py
@@ -0,0 +1,55 @@
+import socket
+import cv2
+import numpy as np
+import pickle
+import rgbd_multicam
+import modules
+from position_encode import PositionalEncodingPermute3D
+
+p_enc_3d = PositionalEncodingPermute3D(11)
+z = torch.zeros((1,11,5,6,4))
+print(p_enc_3d(z).shape) # (1, 11, 5, 6, 4)
+tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_ip = "192.168.1.113"
+server_port = 8088
+tcp_client_socket.connect((server_ip, server_port))
+
+# tobe changed
+capture = cv2.VideoCapture(0)
+cv2.namedWindow('camera', cv2.WINDOW_NORMAL)
+
+try:
+    while capture.isOpened():
+        success, frame = capture.read()
+        # 视频结束
+        bboxes, depth, color = rgbd_multicam.generate_bbox()
+
+        # seg = rgbd_multicam.genarate_seg()
+        track = rgbd_multicam.generate_track()
+        if not success:
+            print('Video Reading Finished!')
+            break
+
+        # 在这里进行图像处理和嵌入
+
+        concat_data = modules.dataconcat(bboxes,depth, color, track)
+        p_enc_3d = PositionalEncodingPermute3D(11)
+        embedding_vector = p_enc_3d(concat_data)
+
+        # embedding_vector = np.random.rand(100)
+
+        embedded_data = pickle.dumps(embedding_vector)
+
+        tcp_client_socket.send(embedded_data)
+        print("Embedding vector sent to server.")
+
+
+        # tcp_client_socket.send(frame.tobytes())
+        # # 接收服务器回传信息
+        # recv_data = tcp_client_socket.recv(1024)
+        # print(recv_data.decode('utf-8'))
+
+finally:
+    capture.release()
+    cv2.destroyAllWindows()
+    tcp_client_socket.close()
---
+++ trackTogether/server.py
@@ -0,0 +1,60 @@
+import socket
+import cv2
+import numpy as np
+import threading
+import modules
+embed_dim = 2764800
+shape = [720, 1280, 3]
+client_data = {}
+global_map = None
+server_ip = ""
+tracker = {}
+def handle_client(client_socket, client_address):
+    global client_data
+    recv_data_whole = bytes()
+    while True:
+        recv_data = client_socket.recv(3000000)  
+        if len(recv_data) == 0:
+            print('disconnect')
+            break
+        else:
+            if client_address not in client_data:
+                client_data[client_address] = bytes()
+            client_data[client_address] += recv_data
+            if len(client_data[client_address]) == embed_dim: 
+#               embedding_vector = pickle.loads(recv_data)
+
+                frame = np.frombuffer(client_data[client_address], dtype=np.uint8).reshape(shape)
+                client_data[client_address] = bytes()
+                # 在此处进行图像拼接或其他处理
+                cv2.imshow(f'camera_received from {client_address}', frame)
+                cv2.waitKey(1)
+                client_socket.send("image has been received!".encode('utf-8'))
+def similarity():
+    # compute similarity and reduce dimention
+    # output: [cla, embed]
+    # https://github.com/MCG-NKU/SERE
+def update_global_map(update_data):
+    # output: [cla, 3dbboxes]
+def start_server():
+    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    address = (server_ip, 8088)
+    tcp_server_socket.bind(address)
+    tcp_server_socket.listen(128)
+
+    while True:
+        client_socket, client_addr = tcp_server_socket.accept()
+        print(f"Connection from {client_addr} has been established!")
+        client_handler = threading.Thread(target=handle_client, args=(client_socket, client_addr))
+        client_handler.start()
+
+
+if __name__ == "__main__":
+    init_server= threading.Thread(target=start_server)
+    if client_data:
+        global_map = update_global_map(similarity())
+        for cla in global_map.keys():
+            tracker[cla] =  modules.tracker()
+
+        tracked_object = tracker[cla].update(global_map[cla])
+    

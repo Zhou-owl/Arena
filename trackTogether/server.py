aa5609addbad65eaa5e840c5dbc90b4e5a36ebb7
Aria <yongqiz2@andrew.cmu.edu>
2024-05-06 20:15:27 -0400

    tempsave

--- trackTogether/server.py
+++ trackTogether/server.py
@@ -31,7 +31,7 @@ def handle_client(client_socket, client_address):
                 cv2.waitKey(1)
                 client_socket.send("image has been received!".encode('utf-8'))
 
-def overlap (box1, box2, thred=0.3):
+def overlap (box1, box2, thred=0.5):
     # box: [x+ x- y+ y- z+ z-]
     # return bool
 def similarity(collected_3ds,self.boxes_list):
@@ -39,11 +39,14 @@ def similarity(collected_3ds,self.boxes_list):
     # boxes_list:{class: [conf, [xyz],1][conf, [xyz],0])...
     
     latest_box_list = {} # {class: [conf, [xyz],2]}
+    
     for key, boxes in collected_3ds.items():
         for box in boxes: #[class conf x+ x- y+ y- z+ z-]
             cur_class = box[0]
             pre_boxes = self.boxes_list[cur_class] #[conf, [xyz],2],...,[conf, [xyz],1],...,[conf, [xyz],0],...
+            
             for pb in pre_boxes:#[conf, [xyz],1]
+
                 if overlap(box[2:],pb[1]):
                     # delete pb from boxes_list
                     # 

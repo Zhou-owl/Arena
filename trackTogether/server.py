7aad82426dce42ef5eb7c0f5e766d720786223f7
Aria <yongqiz2@andrew.cmu.edu>
2024-06-10 18:25:58 -0400

    update

--- trackTogether/server.py
+++ trackTogether/server.py
@@ -84,8 +84,6 @@ class Server:
             with self.data_lock:
                 self.select_curr_frame() # move valid frame to cur_boxes_list
                 # self.valid_curr_boxes() # filter valid boxes in cur_boxes_list
-                self.arena_disply()
-
                 self.data_change = False
                 
                 
@@ -110,7 +108,6 @@ class Server:
                         self.cur_boxes_list[cla] = []
                     self.cur_boxes_list[cla].append([client_address, conf, bounders])
     
-    def arena_disply(self):
 
 
     def valid_curr_boxes(self):
@@ -176,20 +173,118 @@ class Server:
 
 
 
+server = Server()
+server.start_server()
+
+from arena import *
+import random
+import time
+import json
+
+
+# connect to arena scene
+scene = Scene(host="arenaxr.org", namespace = "yongqiz2", scene="yolo")
+#@scene.run_forever(interval_ms=200)
+def periodic():
+    # cur_boxes_list -> cla: [[conf, [bounders]],...]
+    cur_boxes = server.cur_boxes_list
+    box_list = []
+    for cla, cla_boxes in cur_boxes.items():
+        for cla_box in cla_boxes:
+            bounder =  cla_box[1]
+            x_length = bounder[1] - bounder[0]
+            x_center = (bounder[1] + bounder[0]) / 2
+            z_length = bounder[3] - bounder[2]
+            z_center = (bounder[3] + bounder[2]) / 2
+            y_length = bounder[5] - bounder[4] # height
+            y_center = (bounder[5] + bounder[4]) / 2
+            box = Box(
+                object_id = str(cla)+str(cla_box[0]),
+                position = (-x_center, y_center, -z_center),
+                scale = (abs(x_length), abs(y_length), abs(z_length)),
+                color = Color(200,0,200),
+                material= Material(opacity=0.2, transparent=True, visible=True),
+                persist=True
+            )
+            box_list.append(box)
+            scene.add_object(box)
+            my_text = Text(
+                object_id=str(cla)+str(cla_box[0]),
+                text=str(cla),
+                align="center",
+                position=(-x_center, y_center+y_length, -z_center),
+                scale=(0.6,0.6,0.6),
+                color=(100,255,255),
+                persist = True
+            )
+
+
+    text_list = []
+    delet_list = []
+    obj_list = scene.all_objects
+
+    '''
+    for k in obj_list.keys():
+        #print(type(obj_list[k]["object_id"]))
+        if(len(obj_list[k]["object_id"]) < 4):
+            delet_list.append(obj_list[k]["object_id"])
+    for i in delet_list:
+        obj = scene.get_persisted_obj(i)
+        scene.delete_object(obj)
+    '''
+        
+    with open("value.txt",'r') as file:
+        box_string = file.readlines()
+        for idx, i in enumerate(box_string):
+            if '|' not in i:
+                continue
+            pos = i.split('|')[0]
+            class_name = i.split('|')[1].split("\n")[0]
+            try:
+                values = pos.split(']')
+                x = float(values[0].split('[')[-1])
+                y = float(values[2].split('[')[-1])
+                z = float(values[1].split('[')[-1])
+            
+                if type(x)==float and type(y)==float and type(z)==float:
+
+                    if class_name in scale_map.keys():
+                        box_size = scale_map[class_name]
+                        box_color = color_map[class_name]
+                    else:
+                        box_size = scale_map["default"]
+                        box_color = color_map["default"]
+
+                    box = Box(
+                        object_id=str(idx), 
+                        position=(-x,y+box_size[1]*0.5,-z), 
+                        scale=box_size,
+                        color = Color(box_color[0],box_color[1],box_color[2]),
+                        material = Material(opacity=0.2, transparent=True, visible=True),
+                        persist=True
+                    )
+                    box_list.append(box)
+                    scene.add_object(box)
+                    my_text = Text(
+                        object_id=str(-idx-1),
+                        text=class_name,
+                        align="center",
+                        position=(-x,y+box_size[1]+0.15,-z),
+                        scale=(0.6,0.6,0.6),
+                        color=(100,255,255),
+                        persist = True
+                    )
+                    text_list.append(my_text)
+                    scene.add_object(my_text)
+            except Exception as e:
+                print(e)
     
+    time.sleep(0.19)
+    for b,t in zip(box_list,text_list):
+        scene.delete_object(b)
+        scene.delete_object(t)
 
-# Usage
-if __name__ == '__main__':
-    server = Server()
-    server.start_server()
-    from arena import *
-    import random
-    import time
-    import json
-    # setup library
-    scene = Scene(host="arenaxr.org", namespace = "yongqiz2", scene="yolo")
-
-    # make a box
+# make a box
 
     scale_map = {
         "chair":(.45,.9,.45),
@@ -204,74 +299,7 @@ if __name__ == '__main__':
 
 
 
-    @scene.run_forever(interval_ms=200)
-    def periodic():
-        box_list = []
-        text_list = []
-        delet_list = []
-        obj_list = scene.all_objects
-
-        '''
-        for k in obj_list.keys():
-            #print(type(obj_list[k]["object_id"]))
-            if(len(obj_list[k]["object_id"]) < 4):
-                delet_list.append(obj_list[k]["object_id"])
-        for i in delet_list:
-            obj = scene.get_persisted_obj(i)
-            scene.delete_object(obj)
-        '''
-            
-        with open("value.txt",'r') as file:
-            box_string = file.readlines()
-            for idx, i in enumerate(box_string):
-                if '|' not in i:
-                    continue
-                pos = i.split('|')[0]
-                class_name = i.split('|')[1].split("\n")[0]
-                try:
-                    values = pos.split(']')
-                    x = float(values[0].split('[')[-1])
-                    y = float(values[2].split('[')[-1])
-                    z = float(values[1].split('[')[-1])
-                
-                    if type(x)==float and type(y)==float and type(z)==float:
-
-                        if class_name in scale_map.keys():
-                            box_size = scale_map[class_name]
-                            box_color = color_map[class_name]
-                        else:
-                            box_size = scale_map["default"]
-                            box_color = color_map["default"]
-
-                        box = Box(
-                            object_id=str(idx), 
-                            position=(-x,y+box_size[1]*0.5,-z), 
-                            scale=box_size,
-                            color = Color(box_color[0],box_color[1],box_color[2]),
-                            material = Material(opacity=0.2, transparent=True, visible=True),
-                            persist=True
-                        )
-                        box_list.append(box)
-                        scene.add_object(box)
-                        my_text = Text(
-                            object_id=str(-idx-1),
-                            text=class_name,
-                            align="center",
-                            position=(-x,y+box_size[1]+0.15,-z),
-                            scale=(0.6,0.6,0.6),
-                            color=(100,255,255),
-                            persist = True
-                        )
-                        text_list.append(my_text)
-                        scene.add_object(my_text)
-                except Exception as e:
-                    print(e)
-        
-        time.sleep(0.19)
-        for b,t in zip(box_list,text_list):
-            scene.delete_object(b)
-            scene.delete_object(t)
-
+    
     
 
 
